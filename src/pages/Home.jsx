f958c821efa5d309976566f414253024b11e8dda
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-15 10:29:59 +1200

    created TopNavBar.jsx component in shared components subfolder inside pages subfolder. Structured basic react page and routed it to home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./shared-components/Footer.jsx";
+import TopNavBar from "./shared-components/TopNavBar.jsx";
 
 export default function home() {
   return (
     <div>
+      <TopNavBar/>
       <Footer />
     </div>
   );
---
+++ src/pages/shared-components/TopNavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TopNavBar() {
+  return (
+    <div>
+        <div>
+            Metro NZ logo
+        </div>
+        <div>
+            Menu options
+        </div>
+    </div>
+  )
+}
