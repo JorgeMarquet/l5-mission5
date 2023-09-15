66a9214a40ef89492d76546e071971ba837c7fc8
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-15 14:42:00 +1200

    sized menu options arrow icon accordingly

--- src/pages/shared-components/TopNavBar.jsx
+++ src/pages/shared-components/TopNavBar.jsx
@@ -12,11 +12,11 @@ export default function TopNavBar() {
         <div className="flex-grow bg-brightGreen" style={{ marginLeft: '90px', marginRight: '190px' }}>
             <ul className="flex justify-between font-bold text-footertextnavyblue text-lg mb-6"> {/* Changed space-x-6 to justify-between */}
                 <li className="cursor-pointer">Home</li>
-                <li className="cursor-pointer flex items-center">Invest <img src={displayListIcon} alt="Icon" className="ml-2"/></li>
-                <li className="cursor-pointer flex items-center">Manage <img src={displayListIcon} alt="Icon" className="ml-2"/></li>
-                <li className="cursor-pointer flex items-center">Rent <img src={displayListIcon} alt="Icon" className="ml-2"/></li>
-                <li className="cursor-pointer flex items-center">Other Services <img src={displayListIcon} alt="Icon" className="ml-2"/></li>
-                <li className="cursor-pointer flex items-center">About us <img src={displayListIcon} alt="Icon" className="ml-2"/></li>
+                <li className="cursor-pointer flex items-center">Invest <img src={displayListIcon} alt="Icon" className="ml-2 h-[13px] w-[15px]"/></li>
+                <li className="cursor-pointer flex items-center">Manage <img src={displayListIcon} alt="Icon" className="ml-2 h-[13px] w-[15px]"/></li>
+                <li className="cursor-pointer flex items-center">Rent <img src={displayListIcon} alt="Icon" className="ml-2 h-[13px] w-[15px]"/></li>
+                <li className="cursor-pointer flex items-center">Other Services <img src={displayListIcon} alt="Icon" className="ml-2 h-[13px] w-[15px]"/></li>
+                <li className="cursor-pointer flex items-center">About us <img src={displayListIcon} alt="Icon" className="ml-2 h-[13px] w-[15px]"/></li>
                 <li className="cursor-pointer">Contact</li>
             </ul>
         </div>
