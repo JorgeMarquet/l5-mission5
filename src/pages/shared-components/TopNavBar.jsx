0746a50bcc84d79d1636317c9ef9dc27745107e7
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-15 15:43:20 +1200

    made first attempt to include other options for sub options menu for  MANAGE, RENT. Encountering issue where clicking one option displays ALL OPTIONS. to fix in next commit

--- src/pages/shared-components/TopNavBar.jsx
+++ src/pages/shared-components/TopNavBar.jsx
@@ -13,12 +13,14 @@ export default function TopNavBar() {
             <div className="relative flex-grow bg-brightGreen" style={{ marginLeft: '70px', marginRight: '190px' }}>
                 <ul className="flex justify-between font-bold text-footertextnavyblue text-lg mb-6">
                     <li className="cursor-pointer">Home</li>
+
+                    {/* List below includes state hook to visualise suboptions menu accordingly */}
                     <li className="cursor-pointer flex items-center">
                         <span onClick={() => setShowDropdown(!showDropdown)}>Invest</span>
                         <img onClick={() => setShowDropdown(!showDropdown)} src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]" />
                         <div 
                             style={{ display: showDropdown ? 'block' : 'none' }}
-                            className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg"
+                            className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold"
                         >
                             <ul>
                                 <li className="p-2 hover:bg-gray-200">Investment Properties</li>
@@ -26,8 +28,39 @@ export default function TopNavBar() {
                             </ul>
                         </div>
                     </li>
-                    <li className="cursor-pointer flex items-center">Manage <img src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]"/></li>
-                    <li className="cursor-pointer flex items-center">Rent <img src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]"/></li>
+
+                    <li className="cursor-pointer flex items-center">
+                        <span onClick={() => setShowDropdown(!showDropdown)}>Manage</span>
+                        <img onClick={() => setShowDropdown(!showDropdown)} src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]" />
+                        <div 
+                            style={{ display: showDropdown ? 'block' : 'none' }}
+                            className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold"
+                        >
+                            <ul>
+                                <li className="p-2 hover:bg-gray-200">Property Management</li>
+                                <li className="p-2 hover:bg-gray-200">Building Management</li>
+                                <li className="p-2 hover:bg-gray-200">Rental Management</li>
+                                <li className="p-2 hover:bg-gray-200">Body Corporate Administration</li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li className="cursor-pointer flex items-center">
+                        <span onClick={() => setShowDropdown(!showDropdown)}>Rent</span>
+                        <img onClick={() => setShowDropdown(!showDropdown)} src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]" />
+                        <div 
+                            style={{ display: showDropdown ? 'block' : 'none' }}
+                            className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold"
+                        >
+                            <ul>
+                                <li className="p-2 hover:bg-gray-200">Tenancy</li>
+                                <li className="p-2 hover:bg-gray-200">Properties for rent</li>
+                                <li className="p-2 hover:bg-gray-200">Tenancy Application</li>
+                            </ul>
+                        </div>
+                    </li>
+
+
                     <li className="cursor-pointer flex items-center">Other Services <img src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]"/></li>
                     <li className="cursor-pointer flex items-center">About us <img src={displayListIcon} alt="Icon" className="ml-2 h-[12px] w-[15px]"/></li>
                     <li className="cursor-pointer">Contact</li>
