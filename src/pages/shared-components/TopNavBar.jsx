111b19b9e9f496f0c827072dc12cdeb0f094b7a5
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-15 13:22:30 +1200

    created 2 div in TopNavBar, one for MetroNZ logo and one for options menu. Flexed divisions and styles the menu options with a basic format

--- src/pages/shared-components/TopNavBar.jsx
+++ src/pages/shared-components/TopNavBar.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 export default function TopNavBar() {
   return (
-    <div>
+    <div className="flex justify-between items-center px-4 py-2"> {/* Added flex for horizontal layout and some padding for spacing */}
         <div>
-            Metro NZ logo
+            {/* Placeholder for Metro NZ logo */}
+            <img src="path_to_your_logo_image" alt="Metro NZ logo" className="h-8 w-auto"/> {/* You can adjust the h-8 to control the height */}
         </div>
         <div>
-            Menu options
+            <ul className="flex space-x-6 font-semibold text-footertextnavyblue text-lg mb-6"> {/* Added flex for horizontal list items and space-x-6 for spacing between items */}
+                <li className="cursor-pointer">Home</li>
+                <li className="cursor-pointer">Invest</li>
+                <li className="cursor-pointer">Manage</li>
+                <li className="cursor-pointer">Rent</li>
+                <li className="cursor-pointer">Other Services</li>
+                <li className="cursor-pointer">About us</li>
+                <li className="cursor-pointer">Contact</li>
+            </ul>
         </div>
     </div>
   )
