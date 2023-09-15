db79e6075c9cc9d5645a513504e7ddced21224ca
Waterclone <jene@missionreadyhq.com>
2023-09-15 15:02:16 +1200

    completed HomeMakeTheChange.jsx component and FindOutButton in shared-components. Also, add 3 images to image folder.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import TopNavBar from "./shared-components/TopNavBar.jsx";
 import Footer from "./shared-components/Footer.jsx";
+import HomeMakeTheChange from "./HomeMakeTheChange.jsx";
 
 export default function home() {
   return (
     <div>
+      <TopNavBar />
+      <HomeMakeTheChange />
       <Footer />
     </div>
   );
---
+++ src/pages/shared-components/TopNavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../images/metroLogo.svg";
+
+export default function TopNavBar() {
+  return (
+    <div className="flex justify-between items-center px-4 py-2 ">
+      <div>
+        <Logo width={497} height={148} />
+      </div>
+      <div className="flex-grow bg-brightGreen" style={{ marginLeft: "90px", marginRight: "190px" }}>
+        <ul className="flex justify-between font-bold text-footertextnavyblue text-lg mb-6">
+          {" "}
+          {/* Changed space-x-6 to justify-between */}
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">Invest</li>
+          <li className="cursor-pointer">Manage</li>
+          <li className="cursor-pointer">Rent</li>
+          <li className="cursor-pointer">Other Services</li>
+          <li className="cursor-pointer">About us</li>
+          <li className="cursor-pointer">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
