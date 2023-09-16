632dd8ff35bddb7b3eac7e970343528e4e62999b
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-16 12:32:08 +1200

    Started working on Services Cards container and elements inside

--- src/pages/HomeMainServices.jsx
+++ src/pages/HomeMainServices.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import BackgroundPhoto from '../images/MainServicesAucklandCitySunset.png';
 import Invest from '../images/houseWithKeys.png';
 import Rent from '../images/rentIcon.png';
+import Manage from '../images/managementIcon.png'
+import Appraisal from '../images/appraisalIcon.png'
 
 export default function HomeMainServices() {
   return (
@@ -24,26 +26,28 @@ export default function HomeMainServices() {
             </div>
         </div>
 
-        {/* MAIN SERVICES CARD */}
-        <div className="Services">
-            <div>
+        {/* MAIN SERVICES CARDS */}
+        <div className="Services Container">
+            <div className="Service Card">
                 <img src={Invest} alt="Invest" />
                 <p>Invest with Us</p>
             </div>
-            <div>
+            <div className="Service Card">
                 <img src={Rent} alt="Rent" />
                 <p>Properties for Rent</p>
             </div>
-            <div>
-                Property Management
+            <div className="Service Card">
+                <img src={Manage} alt="Property Management" />
+                <p>Property Management</p>
             </div>
-            <div>
+            <div className="Service Card">
+                <img src={Appraisal} alt="Property Appraisal" />
                 Property Appraisal
             </div>
         </div>
 
         {/* INSPIRATIONAL QUOTE */}
-        <div>
+        <div className ="Quote">
             <p>Working with you for you</p>
             <p>We offer tailored services</p>
         </div>
