1e52f213e7118f76a79e4939a6b4ba2ca7ddf092
Waterclone <jene@missionreadyhq.com>
2023-09-16 14:37:29 +1200

    Restrained webpage to w-[1440px]. Started carousel work.

--- src/pages/HomeMakeTheChange.jsx
+++ src/pages/HomeMakeTheChange.jsx
@@ -6,25 +6,27 @@ import FindOutButton from "./shared-components/FindOutButton";
 
 export default function HomeMakeTheChange() {
   return (
-    <div className="w-screen h-makelg bg-gradient-to-b from-makegradgrey to-white font-display">
-      <div className="flex justify-center">
+    <div className="flex flex-col items-center min-w-fit">
+      <div className="w-[1440px] h-makelg bg-gradient-to-b from-makegradgrey to-white font-display">
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center">
+            <img className=" mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Awards} alt="" />
+            <h1 className="mt-5 text-4xl font-bold">Awards</h1>
+          </div>
+          <div className="flex flex-col items-center ml-24 mr-16 ">
+            <img className="mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Articles} alt="" />
+            <h1 className="mt-5 text-4xl font-bold">Articles</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <img className="mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Social} alt="" />
+            <h1 className="mt-5 text-4xl font-bold">Social Responsibility</h1>
+          </div>
+        </div>
         <div className="flex flex-col items-center">
-          <img className=" mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Awards} alt="" />
-          <h1 className="mt-5 text-4xl font-bold">Awards</h1>
+          <h1 className="mt-16 text-5xl font-bold">Make the Change</h1>
+          <h1 className="mt-4 text-4xl font-normal">Switch to Metro NZ</h1>
+          <FindOutButton />
         </div>
-        <div className="flex flex-col items-center ml-24 mr-16 ">
-          <img className="mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Articles} alt="" />
-          <h1 className="mt-5 text-4xl font-bold">Articles</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <img className="mt-24 w-72 object-cover rounded-2xl drop-shadow-lg" src={Social} alt="" />
-          <h1 className="mt-5 text-4xl font-bold">Social Responsibility</h1>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <h1 className="mt-16 text-5xl font-bold">Make the Change</h1>
-        <h1 className="mt-4 text-4xl font-normal">Switch to Metro NZ</h1>
-        <FindOutButton />
       </div>
     </div>
   );
--- src/pages/shared-components/Footer.jsx
+++ src/pages/shared-components/Footer.jsx
@@ -4,15 +4,17 @@ import FooterRight from "./FooterRight.jsx";
 
 export default function Footer() {
   return (
-    <footer className="w-screen h-footerlg bg-gradient-to-b from-footergradwhite to-footergradmidgrey">
-      <div className="text-center sm:flex sm:text-left">
-        <div className="h-footerlg sm:basis-1/2">
-          <FooterLeft />
+    <div className="flex flex-col items-center min-w-fit">
+      <footer className="w-[1440px] h-footerlg bg-gradient-to-b from-footergradwhite to-footergradmidgrey">
+        <div className="text-center sm:flex sm:text-left">
+          <div className="h-footerlg sm:basis-1/2">
+            <FooterLeft />
+          </div>
+          <div className="h-footerlg sm:basis-1/2">
+            <FooterRight />
+          </div>
         </div>
-        <div className="h-footerlg sm:basis-1/2">
-          <FooterRight />
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
