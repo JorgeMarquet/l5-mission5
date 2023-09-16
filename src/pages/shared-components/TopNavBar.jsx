085f48a7395319ba22cf99482580a415f5d4491a
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-17 10:25:24 +1200

    changed tenancy to tenancy information nto RENT menu from TopNavBar

--- src/pages/shared-components/TopNavBar.jsx
+++ src/pages/shared-components/TopNavBar.jsx
@@ -58,7 +58,7 @@ export default function TopNavBar() {
               {/* Check if the dropdown name matches the current state */}
               <div style={{ display: showDropdown === "Rent" ? "block" : "none" }} className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold">
                 <ul>
-                  <li className="p-2 hover:bg-gray-200">Tenancy</li>
+                  <li className="p-2 hover:bg-gray-200">Tenancy Information</li>
                   <li className="p-2 hover:bg-gray-200">Properties for rent</li>
                   <li className="p-2 hover:bg-gray-200">Tenancy Application</li>
                 </ul>
