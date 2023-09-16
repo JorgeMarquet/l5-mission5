b9947e7ea5072110a3c59306888003a8c407c072
Waterclone <jene@missionreadyhq.com>
2023-09-17 00:26:32 +1200

    created homecarousel1 and homecarousel2. locked components to 1440px width. some css touches

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import TopNavBar from "./shared-components/TopNavBar.jsx";
 import Footer from "./shared-components/Footer.jsx";
 import HomeMakeTheChange from "./HomeMakeTheChange.jsx";
-import HomeCustReviews from "./HomeCustReviews.jsx";
 import HomeTenantsHomeOwners from "./HomeTenantsHomeOwners.jsx";
 import HomeMainServices from "./HomeMainServices.jsx";
+import HomeCarousel1 from "./HomeCarousel1.jsx";
+import HomeCarousel2 from "./HomeCarousel2.jsx";
 
 export default function home() {
   return (
     <div>
       <TopNavBar />
       <HomeMainServices />
+      <HomeCarousel1 />
       <HomeTenantsHomeOwners />
-      <HomeCustReviews />
+      <HomeCarousel2 />
       <HomeMakeTheChange />
       <Footer />
     </div>
--- src/pages/HomeMainServices.jsx
+++ src/pages/HomeMainServices.jsx
@@ -7,41 +7,43 @@ import Appraisal from "../images/appraisalIcon.png";
 
 export default function HomeMainServices() {
   return (
-    <div>
-      {/* BACKGROUND IMAGE */}
-      <div className="bg-center bg-cover h-[650px] flex items-start justify-start p-10" style={{ backgroundImage: `url(${BackgroundPhoto})` }}>
-        <div className="text-left">
-          <div className="text-black text-7xl">Auckland's</div>
-          <div className="text-black text-5xl font-semibold">most trusted</div>
-          <div className="text-black text-7xl font-bold">Property Experts</div>
-        </div>
-      </div>
-
-      {/* MAIN SERVICES CARDS */}
-      <div className="Services flex justify-center Container bg-gradient-to-b from -d9d7d7 to-white">
-        <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
-          <img className="w-32 h-32" src={Invest} alt="Invest" />
-          <p>Invest with Us</p>
+    <div className="flex flex-col items-center min-w-fit">
+      <div className="w-[1440px]">
+        {/* BACKGROUND IMAGE */}
+        <div className="bg-center bg-cover h-[650px] flex items-start justify-start p-10" style={{ backgroundImage: `url(${BackgroundPhoto})` }}>
+          <div className="text-left">
+            <div className="text-black text-7xl">Auckland's</div>
+            <div className="text-black text-5xl font-semibold">most trusted</div>
+            <div className="text-black text-7xl font-bold">Property Experts</div>
+          </div>
         </div>
 
-        <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
-          <img className="w-32 h-32" src={Rent} alt="Rent" />
-          <p>Properties for Rent</p>
-        </div>
-        <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
-          <img className="w-32 h-32" src={Manage} alt="Property Management" />
-          <p>Property Management</p>
-        </div>
-        <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
-          <img className="w-32 h-32" src={Appraisal} alt="Property Appraisal" />
-          <p>Property Appraisal</p>
-        </div>
-      </div>
+        {/* MAIN SERVICES CARDS */}
+        <div className="Services flex justify-center Container bg-gradient-to-b from -d9d7d7 to-white">
+          <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
+            <img className="w-32 h-32" src={Invest} alt="Invest" />
+            <p>Invest with Us</p>
+          </div>
 
-      {/* INSPIRATIONAL QUOTE */}
-      <div className="Quote border-t border-b border-black flex flex-col items-center justify-center py-10">
-        <div className="text-black text-6xl font-bold">Working with you, for you. </div>
-        <div className="text-black text-5xl font-semibold text-center leading-relaxed">We offer tailored services</div>
+          <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
+            <img className="w-32 h-32" src={Rent} alt="Rent" />
+            <p>Properties for Rent</p>
+          </div>
+          <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
+            <img className="w-32 h-32" src={Manage} alt="Property Management" />
+            <p>Property Management</p>
+          </div>
+          <div className="Service Card flex flex-col items-center border border-gray-300 rounded-lg p-16 mt-4 mb-5 ml-3 mr-3">
+            <img className="w-32 h-32" src={Appraisal} alt="Property Appraisal" />
+            <p>Property Appraisal</p>
+          </div>
+        </div>
+
+        {/* INSPIRATIONAL QUOTE */}
+        <div className="Quote border-t border-b border-black flex flex-col items-center justify-center py-10">
+          <div className="text-black text-6xl font-bold">Working with you, for you. </div>
+          <div className="text-black text-5xl font-semibold text-center leading-relaxed">We offer tailored services</div>
+        </div>
       </div>
     </div>
   );
