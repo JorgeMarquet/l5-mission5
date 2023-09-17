6693d87a198d6eee097c218368155a2c3f6d9640
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-17 15:19:07 +1200

    created TenancyInformation page route in App.js. Created TenancyInformation jsx file under pages and imported topNavBar and footer to start the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import "./App.css";
+import TenancyInformation from "./pages/TenancyInformation.jsx";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/tenancy-information" element={<TenancyInformation/>}/>
       </Routes>
     </div>
   );
---
+++ src/pages/TenancyInformation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TopNavBar from './shared-components/TopNavBar'
+import Footer from './shared-components/Footer'
+
+export default function TenancyInformation() {
+  return (
+    <div>
+        <div>
+            <TopNavBar/>
+            <Footer/>
+        </div>
+
+    </div>
+  )
+}
