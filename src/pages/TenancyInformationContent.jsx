7bbedb181f913d24a537cf2d79d9b92e7aa6eae2
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-18 16:03:25 +1200

    added button to view property listings

--- src/pages/TenancyInformationContent.jsx
+++ src/pages/TenancyInformationContent.jsx
@@ -6,20 +6,22 @@ export default function TenancyInformationContent() {
     <div>
         {/* BACKGROUND PHOTO */}
         <div className="bg-center bg-cover h-[650px] flex items-center justify-start p-10" style={{ backgroundImage: `url(${BackgroundPhoto})` }}>
-          <div className="text-left">
-            <div className="text-white text-7xl">Finding</div>
-            <div className="text-white text-7xl font-semibold">the property</div>
-            <div className="text-white text-7xl font-bold">that's right for YOU</div>
+          <div className="flex flex-col items-start ml-16"> {/* Added flex container with vertical direction */}
+            <div className="text-left mb-4"> {/* Added some margin bottom for spacing */}
+              <div className="text-white text-7xl">Finding</div>
+              <div className="text-white text-7xl font-semibold">the property</div>
+              <div className="text-white text-7xl font-bold">that's right for YOU</div>
+            </div>
+            <button
+            className="w-[488px] h-[58px] bg-themered rounded-2xl text-white text-xl font-bold  active:bg-violet-700"
+            style={{
+              boxShadow: "0 3px 8px #595959",
+              borderColor: "#808080",
+            }}
+            >
+            View our property listings
+            </button>
           </div>
-        <button
-        className="w-[688px] h-[48px] bg-themered rounded-2xl text-white text-xl font-bold  active:bg-violet-700"
-        style={{
-          boxShadow: "0 5px 8px #c2c0c0",
-          borderColor: "#808080",
-        }}
-        >
-        View our property listings
-        </button>
         </div>
     </div>
   )
