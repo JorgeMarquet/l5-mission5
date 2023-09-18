a1f22dbffbacbbdb25a0a25de3fdc1664bfe3f2c
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-19 09:43:47 +1200

    made changes to TopNavBar in development branch to make it compatible with feature-jorge branch

--- frontend/src/pages/shared-components/TopNavBar.jsx
+++ frontend/src/pages/shared-components/TopNavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ReactComponent as Logo } from "../../images/metroLogo.svg";
 import displayListIcon from "../../images/displayListIcon.png";
-import { Link } from "react-router-dom";
+import  { Link } from "react-router-dom"
 
 export default function TopNavBar() {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -14,7 +14,7 @@ export default function TopNavBar() {
         </div>
         <div className="relative flex-grow bg-brightGreen" style={{ marginLeft: "40px", marginRight: "50px" }}>
           <ul className="flex justify-between font-bold text-footertextnavyblue text-lg mb-6">
-            <li className="font-regular text-sm mt-16 cursor-pointer">
+            <li className="font-regular mt-16 cursor-pointer">
               <Link to="/">Home</Link>
             </li>
 
@@ -23,7 +23,7 @@ export default function TopNavBar() {
             {/*INVEST*/}
             <li className="cursor-pointer flex items-center">
               {/* Toggle the dropdown based on its name */}
-              <span className="font-regular text-sm mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Invest" ? "Invest" : null)}>
+              <span className="font-regular mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Invest" ? "Invest" : null)}>
                 Invest
               </span>
               <img onClick={() => setShowDropdown(showDropdown !== "Invest" ? "Invest" : null)} src={displayListIcon} alt="Icon" className="mt-16 ml-2 h-[9px] w-[12px]" />
@@ -31,8 +31,8 @@ export default function TopNavBar() {
               {/* Check if the dropdown name matches the current state */}
               <div style={{ display: showDropdown === "Invest" ? "block" : "none" }} className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold">
                 <ul>
-                  <li className="text-xs p-2 hover:bg-gray-200">Investment Properties</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Tenanted Property Sales</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Investment Properties</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Tenanted Property Sales</li>
                 </ul>
               </div>
             </li>
@@ -40,7 +40,7 @@ export default function TopNavBar() {
             {/*MANAGE*/}
             <li className="cursor-pointer flex items-center">
               {/* Toggle the dropdown based on its name */}
-              <span className="font-regular text-sm mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Manage" ? "Manage" : null)}>
+              <span className="font-regular mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Manage" ? "Manage" : null)}>
                 Manage
               </span>
               <img onClick={() => setShowDropdown(showDropdown !== "Manage" ? "Manage" : null)} src={displayListIcon} alt="Icon" className="mt-16 ml-2 h-[9px] w-[12px]" />
@@ -48,10 +48,10 @@ export default function TopNavBar() {
               {/* Check if the dropdown name matches the current state */}
               <div style={{ display: showDropdown === "Manage" ? "block" : "none" }} className="absolute top-full mt-1 left-130 w-52 bg-white border rounded-xl shadow-lg text-base font-semibold">
                 <ul>
-                  <li className="text-xs p-2 hover:bg-gray-200">Property Management</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Building Management</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Rental Management</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Body Corporate Administration</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Property Management</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Building Management</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Rental Management</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Body Corporate Administration</li>
                 </ul>
               </div>
             </li>
@@ -59,7 +59,7 @@ export default function TopNavBar() {
             {/*RENT*/}
             <li className="cursor-pointer flex items-center">
               {/* Toggle the dropdown based on its name */}
-              <span className="font-regular text-sm mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Rent" ? "Rent" : null)}>
+              <span className="font-regular mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Rent" ? "Rent" : null)}>
                 Rent
               </span>
               <img onClick={() => setShowDropdown(showDropdown !== "Rent" ? "Rent" : null)} src={displayListIcon} alt="Icon" className="mt-16 ml-2 h-[9px] w-[12px]" />
@@ -81,7 +81,7 @@ export default function TopNavBar() {
             {/*OTHER SERVICES*/}
             <li className="cursor-pointer flex items-center">
               {/* Toggle the dropdown based on its name */}
-              <span className="font-regular text-sm mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Other Services" ? "Other Services" : null)}>
+              <span className="font-regular mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "Other Services" ? "Other Services" : null)}>
                 Other Services
               </span>
               <img onClick={() => setShowDropdown(showDropdown !== "Other Services" ? "Other Services" : null)} src={displayListIcon} alt="Icon" className="mt-16 ml-2 h-[9px] w-[12px]" />
@@ -89,9 +89,9 @@ export default function TopNavBar() {
               {/* Check if the dropdown name matches the Other Services state */}
               <div style={{ display: showDropdown === "Other Services" ? "block" : "none" }} className="absolute top-full mt-1 left-130 w-56 bg-white border rounded-xl shadow-lg text-base font-semibold">
                 <ul>
-                  <li className="text-xs p-2 hover:bg-gray-200">Healthy Homes Standards</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Decluttering Services</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Renovation Project Management</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Healthy Homes Standards</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Decluttering Services</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Renovation Project Management</li>
                 </ul>
               </div>
             </li>
@@ -99,7 +99,7 @@ export default function TopNavBar() {
             {/*ABOUT US*/}
             <li className="cursor-pointer flex items-center">
               {/* Toggle the dropdown based on its name */}
-              <span className="font-regular text-sm mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "About Us" ? "About Us" : null)}>
+              <span className="font-regular mt-16 cursor-pointer" onClick={() => setShowDropdown(showDropdown !== "About Us" ? "About Us" : null)}>
                 About Us
               </span>
               <img onClick={() => setShowDropdown(showDropdown !== "About Us" ? "About Us" : null)} src={displayListIcon} alt="Icon" className="mt-16 ml-2 h-[9px] w-[12px]" />
@@ -107,15 +107,15 @@ export default function TopNavBar() {
               {/* Check if the dropdown name matches the About Us state */}
               <div style={{ display: showDropdown === "About Us" ? "block" : "none" }} className="absolute top-full mt-1 left-130 w-48 bg-white border rounded-xl shadow-lg text-base font-semibold">
                 <ul>
-                  <li className="text-xs p-2 hover:bg-gray-200">Our Management Team</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Gallery</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Work with us</li>
-                  <li className="text-xs p-2 hover:bg-gray-200">Dispute Process</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Our Management Team</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Gallery</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Work with us</li>
+                  <li className="text-bs p-2 hover:bg-gray-200">Dispute Process</li>
                 </ul>
               </div>
             </li>
 
-            <li className="font-regular text-sm mt-16 cursor-pointer">Contact</li>
+            <li className="font-regular mt-16 cursor-pointer">Contact</li>
           </ul>
         </div>
       </div>
