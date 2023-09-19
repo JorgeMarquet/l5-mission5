235f86c09dd9724426df72d09cfbee9eb701554b
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-20 10:09:33 +1200

    changes to listing cards before attempting to connect to backend

--- frontend/src/pages/PropertiesListingsCards.jsx
+++ frontend/src/pages/PropertiesListingsCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 import Listing1 from '../images/listing1.png';
