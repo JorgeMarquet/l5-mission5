dade6e3cfc6966aeb9ec7555a722037317c15751
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-19 12:21:42 +1200

    adjusted sizing settings of cards so they do not get resized upon passing from one image to another

--- frontend/src/pages/PropertiesListingsCards.jsx
+++ frontend/src/pages/PropertiesListingsCards.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css"; // For default styling
+import "react-alice-carousel/lib/alice-carousel.css";
 import Listing1 from '../images/listing1.png';
 import Listing2 from '../images/listing2.png';
 import Listing3 from '../images/listing3.png';
@@ -8,13 +8,12 @@ import Listing4 from '../images/listing4.png';
 
 export default function PropertiesListings() {
   const [currentPage, setCurrentPage] = useState(1);
-
-  const allCards = Array(6).fill().map((_, index) => <PropertiesListingsCards key={index}/>); // Example with 7 cards
+  const allCards = Array(24).fill().map((_, index) => <PropertiesListingsCards key={index}/>);
 
   const cardsPerPage = 6;
   const startIdx = (currentPage - 1) * cardsPerPage;
   const cardsToShow = allCards.slice(startIdx, startIdx + cardsPerPage);
-  
+
   const totalPages = Math.ceil(allCards.length / cardsPerPage);
 
   const handlePageChange = (pageNum) => {
@@ -37,13 +36,12 @@ export default function PropertiesListings() {
 
 function PropertiesListingsCards() {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const handleSlideChange = (event) => {
     setCurrentIndex(event.item);
   };
 
   return (
-    <div className="rounded-lg border border-black overflow-hidden w-1/4 h-96 w-80 m-4">
+    <div className="rounded-lg border border-black overflow-hidden w-1/4 h-96 w-96 m-4">
       <ImageCarousel currentIndex={currentIndex} onSlideChange={handleSlideChange} />
       <ListingInfo />
     </div>
@@ -51,12 +49,7 @@ function PropertiesListingsCards() {
 }
 
 function ImageCarousel({ currentIndex, onSlideChange }) {
-    const images = [
-      Listing1,
-      Listing2,
-      Listing3,
-      Listing4,
-    ];
+    const images = [ Listing1, Listing2, Listing3, Listing4 ];
     const carousel = React.useRef(null);
 
     const responsive = {
@@ -65,7 +58,7 @@ function ImageCarousel({ currentIndex, onSlideChange }) {
     };
 
     return (
-      <div className="relative w-full h-64 overflow-hidden">
+      <div className="relative w-full h-2/3 overflow-hidden">
         <AliceCarousel
           ref={carousel}
           itemsCountPerPage={1}
@@ -89,14 +82,13 @@ function ImageCarousel({ currentIndex, onSlideChange }) {
     );
 }
 
-
 function ListingInfo() {
   const title = "Listing Title";
   const numbers = ["123", "456", "789"];
   const pricePerWeek = "1000/Week";
 
   return (
-    <div className="p-4">
+    <div className="p-4 h-1/3">
       <h2 className="text-xl font-bold">{title}</h2>
       <div className="flex space-x-4 mt-2">
         {numbers.map((num, index) => (
