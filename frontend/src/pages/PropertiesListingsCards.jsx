f466cb06950384f572f269c586c565d7a0fbb4c9
Jorge Arturo Marquet González <jorge@missionreadyhq.com>
2023-09-19 18:50:01 +1200

    added icons and configuration rules to listing cards

--- frontend/src/pages/PropertiesListingsCards.jsx
+++ frontend/src/pages/PropertiesListingsCards.jsx
@@ -5,6 +5,9 @@ import Listing1 from '../images/listing1.png';
 import Listing2 from '../images/listing2.png';
 import Listing3 from '../images/listing3.png';
 import Listing4 from '../images/listing4.png';
+import BedroomIcon from '../images/bed.png';
+import BathroomIcon from '../images/Bathroom.png';
+import CarParkIcon from '../images/CarParkIcon.png';
 
 export default function PropertiesListings() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -65,25 +68,27 @@ function ImageCarousel({ currentIndex, onSlideChange }) {
     };
 
     return (
-      <div className="relative w-full h-2/3 overflow-hidden">
-        <AliceCarousel
-          ref={carousel}
-          itemsCountPerPage={1}
-          responsive={responsive}
-          activeIndex={currentIndex}
-          onSlideChanged={onSlideChange}
-          items={images.map((src, index) => (
-            <div className="w-full h-64 flex items-center justify-center">
-              <img 
-                src={src} 
-                alt={`Slide ${index + 1}`} 
-                key={index} 
-                className="w-full h-full object-cover" 
-                />
-            </div>
-          ))}
-          controlsStrategy="responsive"
-        />
+        <div className="relative w-full h-2/3 overflow-hidden">
+          <AliceCarousel
+            ref={carousel}
+            itemsCountPerPage={1}
+            responsive={responsive}
+            activeIndex={currentIndex}
+            onSlideChanged={onSlideChange}
+            items={images.map((src, index) => (
+              <div className="w-full h-64 flex items-center justify-center">
+                <img 
+                  src={src} 
+                  alt={`Slide ${index + 1}`} 
+                  key={index} 
+                  className="w-full h-full object-cover" 
+                  />
+              </div>
+            ))}
+            controlsStrategy="none"
+            controlsDisabled={true}
+          />
+
         <div className="absolute inset-y-0 left-0 flex items-center z-10">
           <button className="p-2 bg-black bg-opacity-50 text-white" onClick={() => carousel.current.slidePrev()}>‹</button>
         </div>
@@ -94,23 +99,41 @@ function ImageCarousel({ currentIndex, onSlideChange }) {
     );
 }
 
-function ListingInfo() {
-  const title = "Listing Title";
-  const numbers = ["123", "456", "789"];
-  const pricePerWeek = "1000/Week";
+function getFontSizeClass(title) {
+    if (title.length <= 30) return "text-xl";
+    if (title.length <= 45) return "text-lg";
+    if (title.length <= 50) return "text-base";
+    return "text-sm"; // for longer titles
+  }
+  
 
-  return (
-    <div className="p-4 h-1/3">
-      <h2 className="text-xl font-bold">{title}</h2>
-      <div className="flex space-x-4 mt-2">
-        {numbers.map((num, index) => (
-          <span key={index}>{num}</span>
-        ))}
+  function ListingInfo() {
+    const title = "2 Beach Road, Luxury Apartments"; // example title
+    const pricePerWeek = "1000/Week";
+  
+    const details = [
+      { icon: BedroomIcon, number: 3 },
+      { icon: BathroomIcon, number: 2 },
+      { icon: CarParkIcon, number: 1 }
+    ];
+  
+    const titleClass = getFontSizeClass(title);
+  
+    return (
+      <div className="p-4 h-1/3">
+        <h2 className={`${titleClass} font-bold`}>{title}</h2>
+        <ul className="flex space-x-4 mt-2">
+          {details.map((detail, index) => (
+            <li key={index} className="flex items-center font-bold text-gray-500">
+              <img src={detail.icon} alt="" className="h-6 w-6 mr-2"/> {detail.number}
+            </li>
+          ))}
+        </ul>
+        <div className="mt-2 text-xl font-bold">{pricePerWeek}</div>
       </div>
-      <div className="mt-2">{pricePerWeek}</div>
-    </div>
-  );
-}
+    );
+  }
+  
 
 function Pagination({ totalPages, currentPage, onPageChange }) {
   return (
